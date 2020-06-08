4a6943185d307c558b5182bb459cac0e54bafdae
Sander Moolin <sandermoolin@gmail.com>
2020-06-08 12:37:40 -0400

    Adding Select and Text slices

---
+++ prismic/form/Select.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="prismic-form-select">
+        <label :for="cmpId" v-if="label">{{ label }}</label>
+
+        <select :name="cmpId" :id="cmpId">
+            <option value="" selected disabled v-if="placeholder" key="default">
+                {{ placeholder }}
+            </option>
+            <option v-for="({ option }, i) in items" :key="i">
+                {{ option }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+import { kebabCase } from 'lodash'
+export default {
+    props: {
+        label: {
+            type: String,
+            default: ''
+        },
+        placeholder: {
+            type: String,
+            default: ''
+        },
+        items: {
+            type: Array,
+            default: () => []
+        }
+    },
+    computed: {
+        cmpId() {
+            return kebabCase(this.label)
+        }
+    },
+    methods: {
+        kebabCase
+    }
+}
+</script>
+
+<style lang="scss">
+.prismic-form-select {
+}
+</style>
---
+++ prismic/form/Text.vue
@@ -0,0 +1,22 @@
+<template>
+    <prismic-content
+        class="prismic-form-content content-reset"
+        :content="text"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.prismic-form-content {
+}
+</style>
